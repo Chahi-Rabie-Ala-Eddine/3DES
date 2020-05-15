88b56e73df39535f154ba94a85f5acd0d9416927
Ala Eddine Rabie Chahi <32160922+ChahiAladeen@users.noreply.github.com>
2020-05-16 01:00:04 +0200

    upgrading

--- src/DES.cpp
+++ src/DES.cpp
@@ -18,11 +18,14 @@ int main(int argc, char** argv)
     string k1 = "1000011100001111111100001110001111000111000011110001110000111111";
     string k2 = "1000111000011110001110000111111100001110000111111110000111000111";
     string k3 = "1111110000111000111100001110001111000111000011111111000011100001";
+	
+    /*Plaintext*/ 
+    string plaintext = "0123456789ABCDEF";	
     
     /*Encryption*/
-    string ciphertext = TripleDesEnryption("0123456789ABCDEF",k1,k2,k3);
+    string ciphertext = TripleDesEnryption(plaintext,k1,k2,k3);
 	cout << "Encrypt[k1,k2,k3](0123456789ABCDEF) = {" << ciphertext << "}" << endl;
 	
-	/*Decryption*/
-	cout << "Decrypt[k1,k2,k3](" << ciphertext << ") = {" << TripleDesDecryption(ciphertext,k1,k2,k3) << "}" << endl;
+    /*Decryption*/
+    cout << "Decrypt[k1,k2,k3](" << ciphertext << ") = {" << TripleDesDecryption(ciphertext,k1,k2,k3) << "}" << endl;
 }
